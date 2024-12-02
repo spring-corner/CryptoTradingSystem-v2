5e15c93cdf33907d4db02fbf1767d2bec5162355
Teo Jia Cheng <jcteo.2018@scis.smu.edu.sg>
2024-12-03 00:31:45 +0800

    initial commit

---
+++ src/main/java/com/jiachengteo/CryptoTradingSystem_v2/CryptoTradingSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.jiachengteo.CryptoTradingSystem_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingSystemV2Application.class, args);
+	}
+
+}
